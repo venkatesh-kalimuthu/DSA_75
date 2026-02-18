d991c2450bc26c441b1f1529ca1be678e04d8eca
venkatesh-kalimuthu <121718320+venkatesh-kalimuthu@users.noreply.github.com>
2026-02-18 15:21:45 +0530

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+      
+        for (int i = 0; i < nums.length; i++) {
+            int comp = target - nums[i];
+          
+            // Perform the lookup ONCE
+            Integer complementIndex = map.get(comp);
+          
+            if (complementIndex != null) {
+                return new int[] {complementIndex, i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {-1, -1};
+    }
+}
