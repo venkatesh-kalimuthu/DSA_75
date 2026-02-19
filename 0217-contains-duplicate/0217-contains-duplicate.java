c3d1954aecc6cd62631fb1079a803ac7b358161d
venkatesh-kalimuthu <121718320+venkatesh-kalimuthu@users.noreply.github.com>
2026-02-19 19:00:46 +0530

    Update 0217-contains-duplicate.java

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,1 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
 
+        Set<Integer> set = new HashSet<>();
+
+      # using streams will add pipeline objects overhead. For loop is effective here
+        for(int num : nums){
+            if(!set.add(num)){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
